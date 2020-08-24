a6ac18cce007bfa40da64ef89fabf11a622e1212
mojo695 <69871354+mojo695@users.noreply.github.com>
2020-08-24 14:34:22 -0500

    Add files via upload
    yessss

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+//----------------------------
+
+   int USD = 8;
+
+   double EURO = .85;
+
+//----------------------------
+
+   System.out.println(USD + " U.S. dollars converted to euros is " + USD * EURO + " euros");
+  }
+}
